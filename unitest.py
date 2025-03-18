b0c2ef03b300ef95a0e7c52a90a95a6f4afb8331
Katana-lab <bytulk4@gmail.com>
2025-03-18 10:26:09 +0200

    Початковий коміт

--- unitest.py
+++ unitest.py
@@ -1,23 +1,23 @@
 import unittest
 from lab1 import monoton
 
-class TestMonotonicArray(unittest.TestCase):
-    def test_increasing(self):
+class TestMonoton(unittest.TestCase):
+    def test_zrist(self):
         self.assertTrue(monoton([1, 2, 3, 4, 5]))
     
-    def test_decreasing(self):
+    def test_spad(self):
         self.assertTrue(monoton([5, 4, 3, 2, 1]))
     
-    def test_not_monotonic(self):
+    def test_ne_monoton(self):
         self.assertFalse(monoton([1, 2, 2, 3, 2, 4]))
     
-    def test_equal_elements(self):
+    def test_rivni(self):
         self.assertTrue(monoton([3, 3, 3, 3]))
     
-    def test_single_element(self):
+    def test_odunoki(self):
         self.assertTrue(monoton([7]))
     
-    def test_empty_list(self):
+    def test_pusti(self):
         self.assertTrue(monoton([]))
 
 if __name__ == "__main__":
